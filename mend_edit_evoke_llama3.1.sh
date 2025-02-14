d8961a3e2b746e34eec637ab66f5297f1f311a8a
Frankkkk233 <lyh3348198038@gmail.com>
2025-02-14 10:41:14 +0800

    Add files via upload

---
+++ mend_edit_evoke_llama3.1.sh
@@ -0,0 +1,3 @@
+# hparams/MEND/qwen2.5-instruct.yaml:
+# archive: ./results/InstructEdit/evoke/models/MEND/Llama3.1-8B-Instruct
+CUDA_VISIBLE_DEVICES=5 python model_edit.py --ds evoke --alg MEND --model llama3.1-instruct
